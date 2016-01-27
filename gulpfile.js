a6b8b6e202c3faba64ad520524bf6d602318a3db
Erin Meyers <erinmeyers88@gmail.com>
2016-01-27 12:14:11 -0700

    removed jhint and jscs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,30 @@
 var gulp = require('gulp');
+// var jshint = require('gulp-jshint');
+// var jscs = require('gulp-jscs');
+// var util = require('gulp-util');
+// var gulpprint = require('gulp-print');
+// var gulpif = require('gulp-if');
+// var args = require('yargs').argv;
+// var config = require('./gulp.config')();
 
 
-gulp.task('hello-world', function () {
-	console.log('our first task');
-});
+// gulp.task('vet', function () {
+// 	return gulp.src(config.alljs)
+// 		.pipe(gulpif(args.verbose, gulpprint()))
+// 		.pipe(jscs())
+// 		.pipe(jshint())
+// 		.pipe(jshint.reporter('jshint-stylish', {verbose: true}))
+// 		.pipe(jshint.reporter('fail'));
+// });
+
+// function log(msg) {
+// 	if (typeof(msg) === 'object') {
+// 		for (var item in msg) {
+// 			if (msg.hasOwnProperty(item)) {	
+// 				util.log(util.colors.blue(msg[item]));
+// 			}
+// 		}
+// 	} else {
+// 		util.log(util.colors.blue(msg));
+// 	}
+// }
